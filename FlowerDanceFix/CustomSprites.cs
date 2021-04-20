77238734fae5cf04c7abc4550518294c4752775a
Meghan Emily Griffiths <megriffi@uwaterloo.ca>
2021-04-19 23:39:20 -0400

    Branch fix?

--- FlowerDanceFix/CustomSprites.cs
+++ FlowerDanceFix/CustomSprites.cs
@@ -9,36 +9,24 @@ using StardewValley.Network;
 using StardewValley.Objects;
 using StardewModdingAPI;
 using Harmony;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace FlowerDanceFix
 {
     class CustomSprites
     {
-        /*
-
-        //This is all garbage I'm annotating to eventually code for switching sprites, please ignore
-
-        string rawFestivalData = GetFestivalDataForYear("mainEvent");
-			for (int i = 1; i <= 6; i++)
-			{
-				string female2 = ((!females[i - 1].IsVillager()) ? ("farmer" + Utility.getFarmerNumberFromFarmer(females[i - 1].TryGetFarmer())) : females[i - 1].TryGetVillager().Name);
-        // string female2
-        // females[i-1] is not villager? -> is the element in females[i-1] a farmer?
-        //yes: farmer + (females[i-1]'s farmer number), add to string female2
-        //no: females[i-1]'s name, add to string female2
-
-        string male = ((!males[i - 1].IsVillager()) ? ("farmer" + Utility.getFarmerNumberFromFarmer(males[i - 1].TryGetFarmer())) : males[i - 1].TryGetVillager().Name);
-        // string male
-        // males[i-1] is not villager? -> is the element in males[i-1] a farmer?
-        //yes: farmer + (males[i-1]'s farmer number), add to 
-
-
-        rawFestivalData = rawFestivalData.Replace("Girl" + i, female2);
-                //in string rawFestivalData, for each of i, replace placeholder Girl(i) with female dancer's name
-
-				rawFestivalData = rawFestivalData.Replace("Guy" + i, male);
-                //in string rawFestivalData, for each i, replace placeholder Girl(i) with male dancer's name
-        */
+        public static bool HasFDFSprites(NPC character)
+        {
+            try
+            {
+                Game1.temporaryContent.Load<Texture2D>("Characters\\" + NPC.getTextureNameForCharacter(character.name.Value) + "_FDF");
+                return true;
+            }
+            catch (Exception)
+            {
+            }
+            return false;
+        }
     }
 
 }
