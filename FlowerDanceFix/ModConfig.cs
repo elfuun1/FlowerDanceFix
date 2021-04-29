9f464058d62230b549d7a907026ade83b19e46fe
Meghan Emily Griffiths <megriffi@uwaterloo.ca>
2021-04-29 09:46:49 -0400

    latest commit

--- FlowerDanceFix/ModConfig.cs
+++ FlowerDanceFix/ModConfig.cs
@@ -15,8 +15,6 @@ namespace FlowerDanceFix
     public class ModConfig
     {
         public ModConfig Config;
-
-        public bool ExperimentalFeatures { get; set; } = false;
         
         //WIP- Changes number of pairs dancing- not tested for more than 6 pairs
         public int MaxDancePairs { get; set; } = 6;
@@ -25,13 +23,16 @@ namespace FlowerDanceFix
         public bool NPCsHaveRandomPartners { get; set; } = false;
 
         //WIP- Pairs of NPCs dancing are random pairs of random genders- will require additional sprites
-        public bool ForceHeteroPartners { get; set; } = true;
+        public bool MixGenderLines { get; set; } = false;
 
         //WIP- Code to select datables that are not male of female (ie undefined; gender = 2)- will require additional sprites
         public bool AllowNonBinaryPartners { get; set; } = false;
 
         //Can select datables living outside the valley (ie not town; homeRegion != 2)
         public bool AllowTouristPartners { get; set; } = false;
+
+        //Use FDF custom sprites in all circumstances
+        public bool UseCustomFDFSprites { get; set; } = false;
 
         //Configureable blacklist of datables to be removed from genderedList pools- enclose NPC base name in quotes, deliniate forward slash
         public string DancerBlackList { get; set; } = "";
